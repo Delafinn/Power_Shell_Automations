8b09c231d58d54b651d1d083f75e7a7ec8e2ea94
Finn <32446445+Delafinn@users.noreply.github.com>
2023-05-24 09:00:48 -0600

    PingTest.ps1

--- PingTest.ps1
+++ PingTest.ps1
@@ -1,9 +1,11 @@
 $computers = "Computer1", "Computer2", "Computer3"  # Replace with the actual computer names or IP addresses
 
+# $csvFilePath = "C:\path\to\computers.csv"  Replace with the path to your CSV file
+
+$computers = Import-Csv -Path $csvFilePath | Select-Object -ExpandProperty ComputerName
+
 foreach ($computer in $computers) {
-    $pingResult = Test-Connection -ComputerName $computer -Count 1 -Quiet
-    
-    if ($pingResult) {
+    if (Test-Connection -ComputerName $computer -Count 1 -ErrorAction SilentlyContinue) {
         Write-Host "$computer is up" -ForegroundColor Green
     } else {
         Write-Host "$computer is down" -ForegroundColor Red
