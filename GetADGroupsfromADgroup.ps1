4ab14ccc7e30d6d731dd117e3a638efc2def25b6
Finn <32446445+Delafinn@users.noreply.github.com>
2024-02-12 16:41:27 -0700

    Add files via upload

---
+++ GetADGroupsfromADgroup.ps1
@@ -0,0 +1,17 @@
+﻿# gets the permissions related to a AD group
+
+
+
+$username1 = Read-Host "Enter the ADgroup we are grabbing the permissions for."
+
+Write-Host "got it."
+Start-Sleep .5
+Write-Host "Working!!!"
+
+$userpermissions = (Get-ADGroup -Identity "$username1" -Properties MemberOf).MemberOf | Get-ADGroup | Select-Object Name
+
+$userpermissions
+
+Read-Host "Press enter and please copy the below permissions."
+
+Read-Host "Please copy the above permissions and press ente"
