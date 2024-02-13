5e0de49041381e2b24dd0b8d3ffbd9b579abdc29
Finn <32446445+Delafinn@users.noreply.github.com>
2024-02-12 17:05:02 -0700

    Delete Get-UserPermissions.ps1

--- Get-UserPermissions.ps1
+++
@@ -1,8 +0,0 @@
-﻿#prompt the user to enter the username of the user for whom you want to retrieve group membership information 
-
-$username = Read-Host "Provide the username of the user you want permissions from."
-
-$output = Get-ADPrincipalGroupMembership -Identity $username | Select-Object Name
-
-# Use the Get-ADPrincipalGroupMembership cmdlet to retrieve the group membership information for the specified user and select the Name property of the returned objects
-Write-Output $output
